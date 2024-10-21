add13aa29b45bd80787c703ed1f324e433890e53
bchowlur <bchowlur@ufl.edu>
2024-10-21 16:40:08 -0400

    Updated to add the decoder function

--- encoder.py
+++ encoder.py
@@ -12,3 +12,12 @@ def encode(password):
         encoded_password += str(n)
 
     return encoded_password
+
+def decode(encoded_password):
+    decoded_password = "" 
+
+    for n in password:
+        original = (int(n) - 3) % 10
+        decoded_password += str(original)
+
+    return decoded_password
