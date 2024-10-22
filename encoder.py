feeaad26bce471d68615671e02897af45b82193a
bchowlur <bchowlur@ufl.edu>
2024-10-22 18:35:38 -0400

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -13,11 +13,3 @@ def encode(password):
 
     return encoded_password
 
-def decode(encoded_password):
-    decoded_password = "" 
-
-    for n in password:
-        original = (int(n) - 3) % 10
-        decoded_password += str(original)
-
-    return decoded_password
